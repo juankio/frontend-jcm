b05a8a24514412493467b0c8fc7a7d6d90f32ca7
Miguel <juanmiguelruizsupe@gmail.com>
2024-08-24 23:10:09 -0500

    modificacion de los appoint

--- src/components/Appointment.vue
+++ src/components/Appointment.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class=" bg-white p-5 space-y-3 rounded-lg">
+    <div class="bg-white p-5 space-y-3 rounded-lg">
         <p class="text-green-600 font-black">
             Fecha: <span class="font-light text-gray-600">{{ displayDate(appointment.date) }}</span>
-            Hora: <span class="font-light text-gray-600">{{ appointment.time }}</span></p>
+            Hora: <span class="font-light text-gray-600">{{ appointment.time }}</span>
+        </p>
         <p class="text-lg font-black">Servicios Solicitados en la cita</p>
-        <div v-for="service in appointment.services" :key="service.name">
+        <div v-for="service in appointment.serviceDetails" :key="service.name">
             <p>{{ service.name }}</p>
             <p class="text-2xl font-black text-green-600">{{ formatCurrenCy(service.price) }}</p>
         </div>
 
-        <p class="text-2xl font-black text-right"> Total a pagar: <span class="text-green-600">{{
-                formatCurrenCy(appointment.totalAmount) }}</span></p>
+        <p class="text-2xl font-black text-right">
+            Total a pagar: <span class="text-green-600">{{ formatCurrenCy(appointment.totalAmount) }}</span>
+        </p>
 
         <div class="flex gap-2 items-center">
             <RouterLink class="bg-slate-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none"
                 :to="{ name: 'edit-appointment', params: { id: appointment._id } }">
                 Editar Cita
             </RouterLink>
-            <button class="bg-red-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md: flex-none"
+            <button class="bg-red-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none"
                 @click="appointments.cancelAppointment(appointment._id)">
                 Cancelar Cita
             </button>
