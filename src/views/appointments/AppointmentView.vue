76339b1147c18936194c3420abd60c7ea7bb7ec7
Miguel <juanmiguelruizsupe@gmail.com>
2024-08-30 10:03:06 -0500

    chore: Corregir error de ortografía en el componente AppointmentView.vue

--- src/views/appointments/AppointmentView.vue
+++ src/views/appointments/AppointmentView.vue
@@ -5,7 +5,7 @@
             <p class="text-white text-lg">A continuación verifica la información y confirma tu cita</p>
 
             <h3 class="text-3xl font-extrabold text-white">Servicios</h3>
-            <p v-if="appointments.noServicesSelectied" class="text-white text-2xl text-center">No hay servicios
+            <p v-if="appointments.noServicesSelected" class="text-white text-2xl text-center">No hay servicios
                 seleccionados</p>
             <div v-else class="grid gap-5">
                 <SelectedService v-for="service in appointments.services" :key="service._id" :service="service" />
@@ -15,7 +15,7 @@
                 </p>
             </div>
         </div>
-        <div class="space-y-8" v-if="!appointments.noServicesSelectied">
+        <div class="space-y-8" v-if="!appointments.noServicesSelected">
             <h3 class="text-3xl font-extrabold text-white">Fecha y hora</h3>
 
             <div class="lg:flex gap-5 items-start">
